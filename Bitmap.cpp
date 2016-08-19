506a33fb4c88740e90759ddea1479711219e3f26
Toby Jones <ace_roqs@yahoo.com>
2016-08-18 23:52:28 -0700

    Beginning bitmap scaling code.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -36,5 +36,33 @@ static void generate_grid_texture_rgb(
 }
 #endif
 
+#if 0
+Bitmap resize_bitmap(const Bitmap& unscaled_bitmap, unsigned int xsize, unsigned int ysize)
+{
+    Bitmap scaled_bitmap{std::vector<uint8_t>(xsize * ysize * sizeof(Color_rgb)), xsize, ysize, true};
+
+    for(unsigned int y_index = 0; y_index < ysize; ++y_index)
+    {
+        for(unsigned int x_index = 0; x_index < xsize; )
+        {
+            Color_rgb color;
+
+            // TODO: 2016: Point sample the unscaled_bitmap.
+            (void)unscaled_bitmap;
+            color.red = 255;
+            color.green = 255;
+            color.blue = 0;
+
+            scaled_bitmap.bitmap[y_index * ysize + x_index + 0] = color.red;
+            scaled_bitmap.bitmap[y_index * ysize + x_index + 1] = color.green;
+            scaled_bitmap.bitmap[y_index * ysize + x_index + 2] = color.blue;
+            x_index += sizeof(Color_rgb);
+        }
+    }
+
+    return scaled_bitmap;
+}
+#endif
+
 }
 
