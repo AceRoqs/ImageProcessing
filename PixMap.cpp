4af2825963485b0af62fb45b2c4c40a8bd82106d
Toby Jones <ace_roqs@yahoo.com>
2016-08-24 23:39:39 -0700

    Add whitespace parsing.

--- PixMap.cpp
+++ PixMap.cpp
@@ -6,15 +6,39 @@
 // https://en.wikipedia.org/wiki/Netpbm_format
 namespace ImageProcessing
 {
-static const uint8_t* skip_whitespace(const uint8_t* scan_location, size_t size)
+static bool is_ascii_whitespace_character(char ch) noexcept
 {
-    (void)size;
+    const char whitespace[] = { u8'\t', u8'\n', u8'\v', u8'\f', u8'\r', u8' '};
+    for(auto ix = 0u; ix < sizeof(whitespace); ++ix)
+    {
+        if(whitespace[ix] == ch)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static const uint8_t* skip_whitespace(_In_reads_(size) const uint8_t* scan_location, size_t size)
+{
+    for(size_t ix = 0; ix < size; ++ix)
+    {
+        if(is_ascii_whitespace_character(*scan_location))
+        {
+            ++scan_location;
+        }
+        else
+        {
+            break;
+        }
+    }
 
     return scan_location;
 }
 
 
-Bitmap decode_bitmap_from_pixmap_memory(_In_count_(size) const uint8_t* pixmap_memory, size_t size)
+Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_memory, size_t size)
 {
     (void)pixmap_memory;
     (void)size;
