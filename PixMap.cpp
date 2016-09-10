0d7930b8f508965b209b7f8064e5dd74edb11136
Toby Jones <ace_roqs@yahoo.com>
2016-09-09 23:03:20 -0700

    Implement basic parsing loop.

--- PixMap.cpp
+++ PixMap.cpp
@@ -25,7 +25,7 @@ static bool is_ascii_whitespace_character(char ch) noexcept
     return false;
 }
 
-/*static*/ int parse_int(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success) noexcept
+static int parse_int(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success) noexcept
 {
     *success = false;
     *token_end = buffer + size;
@@ -87,7 +87,7 @@ static bool is_ascii_whitespace_character(char ch) noexcept
     return result;
 }
 
-/*static*/ std::string parse_string(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success)
+static std::string parse_string(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success)
 {
     *success = false;
     *token_end = buffer + size;
@@ -137,11 +137,12 @@ static bool is_ascii_whitespace_character(char ch) noexcept
     return result;
 }
 
-/*static*/ void find_end_of_line(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** line_end) noexcept
+static void find_end_of_line(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** line_end) noexcept
 {
     *line_end = buffer + size;
     const char* line_begin = buffer;
 
+    // TODO: 2016: Try to replace this with a call to std::find, and then a second step to check for \r\n.
     for(size_t ix = 0; ix < size; ++ix)
     {
         if(line_begin[ix] == u8'\n')
@@ -168,67 +169,74 @@ static bool is_ascii_whitespace_character(char ch) noexcept
     }
 }
 
-/*static*/ void advance_to_end_of_line(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success) noexcept
-{
-    *token_end = buffer + size;
-    *success = false;
-    const char* token_begin = buffer;
-
-    for(size_t ix = 0; ix < size; ++ix)
-    {
-        if(token_begin[ix] == u8'\n')
-        {
-            *token_end = token_begin + ix + 1;
-            *success = true;
-            break;
-        }
-        else if(token_begin[ix] == u8'\r')
-        {
-            *token_end = token_begin + ix + 1;
-            *success = true;
-
-            // Eat the \n if the file has \r\n as the deliminator as on Windows.
-            ++ix;
-            if(ix < size)
-            {
-                if(token_begin[ix] == u8'\n')
-                {
-                    *token_end = token_begin + ix + 1;
-                }
-            }
-
-            break;
-        }
-    }
-}
-
-void advance_past_whitespace(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** next_token) noexcept
+static void advance_past_whitespace(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** next_token) noexcept
 {
      *next_token = std::find_if_not(buffer, buffer + size, is_ascii_whitespace_character);
  }
 
 Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_memory, size_t size)
 {
-    (void)pixmap_memory;
-    (void)size;
+    const char* line_begin = reinterpret_cast<const char*>(pixmap_memory);
+    const char* line_end = line_begin;
+    const char* buffer_end = line_begin + size;
 
-    // TODO: 2016: Support comment before the magic number
-    auto next_token = reinterpret_cast<const char*>(pixmap_memory);
-    bool success;
+    int width = 0, height = 0, max_value;
 
-    auto magic_number = parse_string(next_token, size, &next_token, &success);
-    CHECK_EXCEPTION(success, u8"Image data is invalid.");
-    CHECK_EXCEPTION(magic_number == u8"P3", u8"Image data is invalid.");
+    enum class Parse_mode {magic, width, height, max_value, data};
+    Parse_mode mode = Parse_mode::magic;
 
-    auto width = parse_int(next_token, size, &next_token, &success);
-    CHECK_EXCEPTION(success, u8"Image data is invalid.");
+    while(line_begin != buffer_end)
+    {
+        if(line_begin == line_end)
+        {
+            find_end_of_line(line_begin, buffer_end - line_begin, &line_end);
+        }
 
-    auto height = parse_int(next_token, size, &next_token, &success);
-    CHECK_EXCEPTION(success, u8"Image data is invalid.");
+        // TODO: 2016: With this line, the parse functions can assume that the buffer they are given is the token start.
+        // TODO: 2016: Normalize size vs buffer_end concept.
+        advance_past_whitespace(line_begin, line_end - line_begin, &line_begin);
+        if(line_begin != line_end)
+        {
+            bool success;
+            if(line_begin[0] == u8'#')
+            {
+                line_begin = line_end;
+            }
+            else if(mode == Parse_mode::magic)
+            {
+                const auto token = parse_string(line_begin, line_end - line_begin, &line_begin, &success);
+                CHECK_EXCEPTION(success, u8"Image data is invalid.");
+                CHECK_EXCEPTION(token == u8"P3", u8"Image data is invalid.");
+                // TODO: Support P1-P6.  Skip P7.
+                mode = Parse_mode::width;
+            }
+            else if(mode == Parse_mode::width)
+            {
+                width = parse_int(line_begin, line_end - line_begin, &line_begin, &success);
+                CHECK_EXCEPTION(success, u8"Image data is invalid.");
 
-    auto max_value = parse_int(next_token, size, &next_token, &success);
-    CHECK_EXCEPTION(success, u8"Image data is invalid.");
-    (void)max_value;
+                mode = Parse_mode::height;
+            }
+            else if(mode == Parse_mode::height)
+            {
+                height = parse_int(line_begin, line_end - line_begin, &line_begin, &success);
+                CHECK_EXCEPTION(success, u8"Image data is invalid.");
+
+                mode = Parse_mode::max_value;
+            }
+            else if(mode == Parse_mode::max_value)
+            {
+                max_value = parse_int(line_begin, line_end - line_begin, &line_begin, &success);
+                CHECK_EXCEPTION(success, u8"Image data is invalid.");
+
+                mode = Parse_mode::data;
+            }
+            else if(mode == Parse_mode::data)
+            {
+                // TODO:
+            }
+        }
+    }
 
     Bitmap bitmap{};
     bitmap.width = width;
