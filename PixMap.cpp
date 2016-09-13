14ba706ccb459d738a688f4936410661fd4c0015
Toby Jones <ace_roqs@yahoo.com>
2016-09-12 21:16:30 -0700

    Normalize size vs. buffer_end concept.

--- PixMap.cpp
+++ PixMap.cpp
@@ -9,36 +9,42 @@ namespace ImageProcessing
 {
 
 constexpr char whitespace[] = {u8'\t', u8'\n', u8'\v', u8'\f', u8'\r', u8' '};
-constexpr char whitespace_and_null[] = {u8'\t', u8'\n', u8'\v', u8'\f', u8'\r', u8' ', u8'\0'};
-constexpr char integers[] = {u8'0', u8'1', u8'2', u8'3', u8'4', u8'5', u8'6', u8'7', u8'8', u8'9'};
 
 static bool is_ascii_whitespace_character(char ch) noexcept
 {
-    for(auto ix = 0u; ix < sizeof(whitespace); ++ix)
-    {
-        if(whitespace[ix] == ch)
-        {
-            return true;
-        }
-    }
-
-    return false;
+    constexpr char const* whitespace_end = whitespace + sizeof(whitespace);
+    return std::find(whitespace, whitespace_end, ch) != whitespace_end;
 }
 
-static int parse_int(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success) noexcept
+static bool is_valid_token_character(char ch) noexcept
+{
+    return ((ch >= u8'0' && ch <= u8'9') ||
+            (ch >= u8'A' && ch <= u8'Z') ||
+            (ch >= u8'a' && ch <= u8'z') ||
+            (ch == u8'#'));
+}
+
+static bool is_valid_integer_character(char ch) noexcept
+{
+    return (ch >= u8'0') && (ch <= u8'9');
+}
+
+// TODO: 2016: When parsing, don't update token_end unless successful.
+
+static int parse_int32(_In_reads_to_ptr_(buffer_end) const char* buffer_start, const char* buffer_end, _Out_ const char** token_end, _Out_ bool* success) noexcept
 {
     *success = false;
-    *token_end = buffer + size;
 
     // Buffer begins with no whitespace.
-    const char* token_begin = buffer;
+    const char* token_begin = buffer_start;
+    *token_end = buffer_end;
 
-    // Nothing to parse if the buffer is only whitespace.
+    // Nothing to parse if the buffer is empty.
     int result = 0;
     if(token_begin != *token_end)
     {
         // The end of the token is the first whitespace character.
-        *token_end = std::find_first_of(token_begin, *token_end, whitespace_and_null, whitespace_and_null + sizeof(whitespace_and_null));
+        *token_end = std::find_first_of(token_begin, *token_end, whitespace, whitespace + sizeof(whitespace));
 
         // Check for negative sign.
         bool negate = false;
@@ -49,11 +55,12 @@ static int parse_int(_In_reads_(size) const char* buffer, size_t size, _Out_ con
         }
 
         // Add the characters into a running sum, if they are digits.
-        while(token_begin != *token_end)
+        while((token_begin != *token_end) && is_valid_integer_character(*token_begin))
         {
-            constexpr char const* integers_end = integers + sizeof(integers);
-            if(std::find(integers, integers_end, *token_begin) == integers_end)
+            // TODO: 2016: Currently, parse_int32 does not support accepting INT_MIN (-2147483648) as an input.
+            if(result > (INT_MAX - (*token_begin - u8'0')) / 10)
             {
+                // Overflow.
                 break;
             }
 
@@ -77,38 +84,30 @@ static int parse_int(_In_reads_(size) const char* buffer, size_t size, _Out_ con
     // If the parse was not successful, do not consume the buffer.
     if(!*success)
     {
-        *token_end = buffer;
+        *token_end = buffer_start;
     }
 
     return result;
 }
 
-static std::string parse_string(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** token_end, _Out_ bool* success)
+static std::string parse_string(_In_reads_to_ptr_(buffer_end) const char* buffer_start, const char* buffer_end, _Out_ const char** token_end, _Out_ bool* success)
 {
     *success = false;
-    *token_end = buffer + size;
 
     // Buffer begins with no whitespace.
-    const char* token_begin = buffer;
+    const char* token_begin = buffer_start;
+    *token_end = buffer_end;
 
-    // Nothing to parse if the buffer is only whitespace.
+    // Nothing to parse if the buffer is empty.
     std::string result;
     if(token_begin != *token_end)
     {
         // The end of the token is the first whitespace character.
-        *token_end = std::find_first_of(token_begin, *token_end, whitespace_and_null, whitespace_and_null + sizeof(whitespace_and_null));
+        *token_end = std::find_first_of(token_begin, *token_end, whitespace, whitespace + sizeof(whitespace));
 
-        // Add the characters into a running sum, if they are digits.
-        while(token_begin != *token_end)
+        // Add the characters into the result buffer.
+        while((token_begin != *token_end) && is_valid_token_character(*token_begin))
         {
-            if(!((*token_begin >= u8'0' && *token_begin <= u8'9') ||
-                 (*token_begin >= u8'A' && *token_begin <= u8'Z') ||
-                 (*token_begin >= u8'a' && *token_begin <= u8'z') ||
-                 (*token_begin == u8'#')))
-            {
-                break;
-            }
-
             result.append(token_begin, 1);
             ++token_begin;
         }
@@ -123,17 +122,17 @@ static std::string parse_string(_In_reads_(size) const char* buffer, size_t size
     // If the parse was not successful, do not consume the buffer.
     if(!*success)
     {
-        *token_end = buffer;
+        *token_end = buffer_start;
     }
 
     return result;
 }
 
-static void find_end_of_line(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** line_end) noexcept
+static void find_first_line_end(_In_reads_to_ptr_(buffer_end) const char* buffer_start, const char* buffer_end, _Out_ const char** line_end) noexcept
 {
     constexpr char newlines[] = {u8'\r', u8'\n'};
-    *line_end = std::find_first_of(buffer, buffer + size, newlines, newlines + sizeof(newlines));
-    if(*line_end != buffer + size)
+    *line_end = std::find_first_of(buffer_start, buffer_end, newlines, newlines + sizeof(newlines));
+    if(*line_end != buffer_end)
     {
         ++(*line_end);
 
@@ -146,9 +145,9 @@ static void find_end_of_line(_In_reads_(size) const char* buffer, size_t size, _
     }
 }
 
-static void advance_past_whitespace(_In_reads_(size) const char* buffer, size_t size, _Out_ const char** next_token) noexcept
+static void find_first_token_begin(_In_reads_to_ptr_(buffer_end) const char* buffer_start, const char* buffer_end, _Out_ const char** token_begin) noexcept
 {
-     *next_token = std::find_if_not(buffer, buffer + size, is_ascii_whitespace_character);
+     *token_begin = std::find_if_not(buffer_start, buffer_end, is_ascii_whitespace_character);
  }
 
 Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_memory, size_t size)
@@ -157,7 +156,7 @@ Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_m
     const char* line_end = line_begin;
     const char* buffer_end = line_begin + size;
 
-    int width = 0, height = 0, max_value;
+    int image_width = 0, image_height = 0, image_max_value;
 
     enum class Parse_mode {magic, width, height, max_value, data};
     Parse_mode mode = Parse_mode::magic;
@@ -166,11 +165,10 @@ Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_m
     {
         if(line_begin == line_end)
         {
-            find_end_of_line(line_begin, buffer_end - line_begin, &line_end);
+            find_first_line_end(line_begin, buffer_end, &line_end);
         }
 
-        // TODO: 2016: Normalize size vs buffer_end concept.
-        advance_past_whitespace(line_begin, line_end - line_begin, &line_begin);
+        find_first_token_begin(line_begin, line_end, &line_begin);
         if(line_begin != line_end)
         {
             bool success;
@@ -180,7 +178,7 @@ Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_m
             }
             else if(mode == Parse_mode::magic)
             {
-                const auto token = parse_string(line_begin, line_end - line_begin, &line_begin, &success);
+                const auto token = parse_string(line_begin, line_end, &line_begin, &success);
                 CHECK_EXCEPTION(success, u8"Image data is invalid.");
                 CHECK_EXCEPTION(token == u8"P3", u8"Image data is invalid.");
                 // TODO: Support P1-P6.  Skip P7.
@@ -190,22 +188,22 @@ Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_m
                     (mode == Parse_mode::height) ||
                     (mode == Parse_mode::max_value))
             {
-                const int token = parse_int(line_begin, line_end - line_begin, &line_begin, &success);
+                const int token = parse_int32(line_begin, line_end, &line_begin, &success);
                 CHECK_EXCEPTION(success, u8"Image data is invalid.");
 
                 if(mode == Parse_mode::width)
                 {
-                    width = token;
+                    image_width = token;
                     mode = Parse_mode::height;
                 }
                 else if(mode == Parse_mode::height)
                 {
-                    height = token;
+                    image_height = token;
                     mode = Parse_mode::max_value;
                 }
                 else
                 {
-                    max_value = token;
+                    image_max_value = token;
                     mode = Parse_mode::data;
                 }
             }
@@ -217,8 +215,8 @@ Bitmap decode_bitmap_from_pixmap_memory(_In_reads_(size) const uint8_t* pixmap_m
     }
 
     Bitmap bitmap{};
-    bitmap.width = width;
-    bitmap.height = height;
+    bitmap.width = image_width;
+    bitmap.height = image_height;
     return bitmap;
 }
 
