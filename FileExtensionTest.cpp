789e79604709a84061a31bf944c2200cb71f9c66
Toby Jones <ace_roqs@yahoo.com>
2016-10-12 22:29:42 -0700

    Enforce extension must be ASCII.

--- FileExtensionTest.cpp
+++ FileExtensionTest.cpp
@@ -1,5 +1,6 @@
 #include "PreCompile.h"
 #include "FileExtensionTest.h"
+#include <PortableRuntime/CheckException.h>
 
 namespace ImageProcessing
 {
@@ -32,13 +33,32 @@ static bool are_utf8_strings_identical_ascii_case_insensitive(_In_z_ const char*
     return identical;
 }
 
+static bool is_utf8_string_ascii(_In_z_ const char* utf8_string) noexcept
+{
+    bool is_ascii = true;
+
+    while(*utf8_string != u8'\0')
+    {
+        if(static_cast<unsigned char>(*utf8_string) >= 128)
+        {
+            is_ascii = false;
+            break;
+        }
+        ++utf8_string;
+    }
+
+    return is_ascii;
+}
+
 // This function does a case sensitive comparison of UTF-8, except that
 // ASCII characters are matched in a case insensitive way.  This is intended, as
 // extensions for image types are all ASCII, and nearly always, the extension is
 // passed as a string literal.  So file names can have UTF-8 characters, but the
 // correct handler (pcx, targa, etc.) will always be correctly found.
-bool file_has_extension_case_sensitive(_In_z_ const char* file_name, _In_z_ const char* extension) noexcept
+bool file_has_extension_case_sensitive(_In_z_ const char* file_name, _In_z_ const char* extension)
 {
+    CHECK_EXCEPTION(is_utf8_string_ascii(extension), u8"File extension must be ASCII.");
+
     const size_t length_file = strlen(file_name);
     const size_t length_extension = strlen(extension);
     return ((length_file >= length_extension) && are_utf8_strings_identical_ascii_case_insensitive(file_name + length_file - length_extension, extension));
--- FileExtensionTest.h
+++ FileExtensionTest.h
@@ -3,7 +3,7 @@
 namespace ImageProcessing
 {
 
-bool file_has_extension_case_sensitive(_In_z_ const char* file_name, _In_z_ const char* extension) noexcept;
+bool file_has_extension_case_sensitive(_In_z_ const char* file_name, _In_z_ const char* extension);
 
 }
 
