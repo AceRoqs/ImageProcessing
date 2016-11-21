ae8cacb0947ce3cf894c6df35ee6f08b088e98ed
Toby Jones <ace_roqs@yahoo.com>
2016-11-20 18:53:49 -0800

    Prep pre-compiled header for Clang.

--- PreCompile.h
+++ PreCompile.h
@@ -3,9 +3,13 @@
 // C++ Standard Library.
 #include <cassert>
 #include <algorithm>
+#include <climits>
 #include <cstdint>
+#include <cstring>
 #include <functional>
 #include <iterator>
 #include <memory>
 #include <vector>
+
+#include <PortableRuntime/StaticAnalysis.h>
 
